9a7fe8faf14e0d065544bc01378d65bd1a8048d4
Jorge A. Villarreal <88699002+jovillarrealm@users.noreply.github.com>
2024-02-15 10:34:46 -0500

    activité physique

--- helloworld_project/pages/urls.py
+++ helloworld_project/pages/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import HomePageView, AboutPageView, ProductIndexView, ProductShowView, ContactPageView
+from .views import HomePageView, AboutPageView, ProductIndexView, ProductShowView, ContactPageView, ProductCreateView, SuccessView
 
 
 urlpatterns = [
@@ -8,5 +8,7 @@ urlpatterns = [
     path("about/", AboutPageView.as_view(), name="about"),
     path("contact/", ContactPageView.as_view(), name="contact"),
     path("products/", ProductIndexView.as_view(), name="products"),
+    path('products/create', ProductCreateView.as_view(), name='form'),
+    path('products/create/success', SuccessView.as_view(), name='success'),
     path("products/<str:id>", ProductShowView.as_view(), name="show"),
 ]
--- helloworld_project/pages/views.py
+++ helloworld_project/pages/views.py
@@ -1,121 +1,154 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.views.generic import TemplateView
 
- 
+from django.http import HttpResponseRedirect
 
- 
+from django import forms
 
-# Create your views here. 
+from django.views import View
+
+# Create your views here.
 class HomePageView(TemplateView):
-    template_name = 'home.html'
-
-class AboutPageView(TemplateView): 
-
-    template_name = 'pages/about.html' 
-
-    def get_context_data(self, **kwargs): 
-
-        context = super().get_context_data(**kwargs) 
-
-        context.update({ 
-
-            "title": "About us - Online Store", 
-
-            "subtitle": "About us", 
-
-            "description": "This is an about page ...", 
-
-            "author": "Developed by: Your Name", 
-
-        }) 
-
- 
-
-        return context 
-
-class ContactPageView(TemplateView): 
-
-    template_name = 'pages/about.html' 
-
-    def get_context_data(self, **kwargs): 
-
-        context = super().get_context_data(**kwargs) 
-
-        context.update({ 
-
-            "title": "Contact us - Online Store", 
-
-            "subtitle": "Contact us", 
-
-            "description": "This is a contact page ...", 
-
-            "author": "Developed by: JVM FakeName", 
-
-        }) 
-
-        return context 
-    
-from django.views import View 
-
- 
-
-class Product: 
-
-    products = [ 
-
-        {"id":"1", "name":"TV", "description":"Best TV"}, 
-
-        {"id":"2", "name":"iPhone", "description":"Best iPhone"}, 
-
-        {"id":"3", "name":"Chromecast", "description":"Best Chromecast"}, 
-
-        {"id":"4", "name":"Glasses", "description":"Best Glasses"} 
-
-    ] 
-
- 
-
-class ProductIndexView(View): 
-
-    template_name = 'products/index.html' 
-
- 
-
-    def get(self, request): 
-
-        viewData = {} 
-
-        viewData["title"] = "Products - Online Store" 
-
-        viewData["subtitle"] =  "List of products" 
-
-        viewData["products"] = Product.products 
-
- 
-
-        return render(request, self.template_name, viewData) 
-
- 
-
-class ProductShowView(View): 
-
-    template_name = 'products/show.html' 
+    template_name = "home.html"
 
 
-    def get(self, request, id): 
+class AboutPageView(TemplateView):
+    template_name = "pages/about.html"
 
-        viewData = {} 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
 
-        product = Product.products[int(id)-1] 
+        context.update(
+            {
+                "title": "About us - Online Store",
+                "subtitle": "About us",
+                "description": "This is an about page ...",
+                "author": "Developed by: JVM FakeName",
+            }
+        )
 
-        viewData["title"] = product["name"] + " - Online Store" 
+        return context
 
-        viewData["subtitle"] =  product["name"] + " - Product information" 
 
-        viewData["product"] = product 
+class ContactPageView(TemplateView):
+    template_name = "pages/about.html"
 
- 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context.update(
+            {
+                "title": "Contact us - Online Store",
+                "subtitle": "Contact us",
+                "description": "This is a contact page ...",
+                "author": "Developed by: JVM FakeName",
+            }
+        )
+
+        return context
+
+
+
+
+
+class Product:
+    products = [
+        {"id": "1", "name": "TV", "description": "Best TV", "price": 100},
+        {"id": "2", "name": "iPhone", "description": "Best iPhone", "price": 200},
+        {
+            "id": "3",
+            "name": "Chromecast",
+            "description": "Best Chromecast",
+            "price": 10,
+        },
+        {"id": "4", "name": "Glasses", "description": "Best Glasses", "price": 101},
+    ]
+
+
+class ProductIndexView(View):
+    template_name = "products/index.html"
+
+    def get(self, request):
+        viewData = {}
+
+        viewData["title"] = "Products - Online Store"
+
+        viewData["subtitle"] = "List of products"
+
+        viewData["products"] = Product.products
 
         return render(request, self.template_name, viewData)
+
+
+class ProductShowView(View):
+    template_name = "products/show.html"
+
+    def get(self, request, id):
+        viewData = {}
+        try:
+            product = Product.products[int(id) - 1]
+        except IndexError:
+            return redirect("/")
+
+        viewData["title"] = product["name"] + " - Online Store"
+
+        viewData["subtitle"] = product["name"] + " - Product information"
+
+        viewData["product"] = product
+
+        viewData["expensive"] = product["price"] > 100
+
+        return render(request, self.template_name, viewData)
+
+
+class ProductForm(forms.Form):
+    name = forms.CharField(required=True)
+
+    price = forms.FloatField(required=True)
+
+    def clean_price(self):
+        price = self.cleaned_data["price"]
+        if price < 0:
+            raise forms.ValidationError("El valor debería ser un `float` positivo :(", "invalid")
+        return price
+            
+
+
+class ProductCreateView(View):
+    template_name = "products/create.html"
+
+    def get(self, request):
+        form = ProductForm()
+
+        viewData = {}
+
+        viewData["title"] = "Create product"
+
+        viewData["form"] = form
+
+        return render(request, self.template_name, viewData)
+
+    def post(self, request):
+        form = ProductForm(request.POST)
+
+        if form.is_valid():
+            return render(request, "products/confim.html")
+
+        else:
+            viewData = {}
+
+            viewData["title"] = "Create product"
+
+            viewData["form"] = form
+
+            return render(request, self.template_name, viewData)
+
+class SuccessView(View):
+    template_name = "products/confirm.html"
+
+    def post(self, request):
+
+        return render(request, self.template_name, {})
